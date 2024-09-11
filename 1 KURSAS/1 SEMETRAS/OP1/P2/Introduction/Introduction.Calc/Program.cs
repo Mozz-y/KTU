a78969985982c13de3f4db8c3cd699bd454213be
Dovydas Lenkas <61661124+Mozz-y@users.noreply.github.com>
2024-09-11 23:57:58 +0300

    Add files via upload

---
+++ 1 KURSAS/1 SEMETRAS/OP1/P2/Introduction/Introduction.Calc/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Introduction.Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite pirmaa skaiciu (a):");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Iveskite antra skaiciu (b):");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Iveskite pageidaujamos matematines operacijos zenkla (+, -, *, /):");
+            char operation = Console.ReadKey().KeyChar;
+
+            Console.WriteLine(); // Adding a blank line for readability
+
+            // Switch case to handle different operations
+            switch (operation)
+            {
+                case '+':
+                    Console.WriteLine($"Rezultatas: {a} + {b} = {a + b}");
+                    break;
+                case '-':
+                    Console.WriteLine($"Rezultatas: {a} - {b} = {a - b}");
+                    break;
+                case '*':
+                    Console.WriteLine($"Rezultatas: {a} * {b} = {a * b}");
+                    break;
+                case '/':
+                    if (b != 0)
+                    {
+                        Console.WriteLine($"Rezultatas: {a} / {b} = {a / b}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Dalyba is nulio negalima");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Netinkamas operacijos zenklas. Pasirinkite +, -, *, arba /");
+                    break;
+            }
+        }
+    }
+}
---
+++ 1 KURSAS/1 SEMETRAS/OP1/P2/Introduction/Introduction.Function/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace Introduction.Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double functionResult;
+            double x;
+            Console.WriteLine("Įveskite x reikšmę:");
+            x = double.Parse(Console.ReadLine());
+            if (x >= 0 && x <= -1)
+                functionResult = Math.Cos(Math.Pow(x, 2));
+            else if (x < 1 && x > 0)
+                functionResult = 4 * Math.Pow(x, 2) + 3;
+            else
+                functionResult = Math.Sqrt(Math.Pow(x, 2) * x + 4);
+            Console.WriteLine(" fx = {0}", functionResult);
+            Console.ReadKey();
+        }
+    }
+}
