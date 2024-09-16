5e98047629c14c6e8760edca10645e8c831383e5
Dovydas Lenkas <61661124+Mozz-y@users.noreply.github.com>
2024-09-16 11:30:09 +0300

    Add files via upload

---
+++ 1 KURSAS/1 SEMETRAS/OP1/P2/Introduction/Introduction.Calc/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Introduction.Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Įveskite pirma skaičių (a):");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Įveskite antra skaičių (b):");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Įveskite pageidaujamos matematinės operacijos ženklą (+, -, *, /):");
+            char operation = Console.ReadKey().KeyChar;
+
+            Console.WriteLine(); // Blank line for space
+
+            switch (operation)
+            {
+                case '+':
+                    Console.WriteLine($"Rezultatas: {a} + {b} = {a + b}");
+                    break;
+                case '-':
+                    Console.WriteLine($"Rezultatas: {a} - {b} = {a - b}");
+                    break;
+                case '*':
+                    Console.WriteLine($"Rezultatas: {a} * {b} = {a * b}");
+                    break;
+                case '/':
+                    if (b != 0)
+                    {
+                        Console.WriteLine($"Rezultatas: {a} / {b} = {a / b}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Dalyba iš nulio negalima");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Netinkamas operacijos ženklas. Pasirinkite +, -, *, arba /");
+                    break;
+            }
+        }
+    }
+}
---
+++ 1 KURSAS/1 SEMETRAS/OP1/P2/Introduction/Introduction.Symbols/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Introduction.If.Step2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char character;
+            Console.WriteLine("Įveskite spausdinamą simbolį");
+            character = Console.ReadLine()[0];
+
+            // Print 10 lines in total (50/5 = 10)
+            for (int i = 0; i < 10; i++)
+            {
+                /// Print 5 symbols per line
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(character);
+                }
+                /// Move to the next line
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
