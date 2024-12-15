ea825d2abebc2f50d2f9ea4d50617e58ca145a36
Dovydas Lenkas <61661124+Mozz-y@users.noreply.github.com>
2024-12-16 01:20:15 +0200

    Add files via upload

---
+++ 1 KURSAS/1 SEMETRAS/OP1/L5/U5-17/U5-17/U5-17/MusicQuestion.cs
@@ -0,0 +1,12 @@
+﻿namespace U5_17
+{
+    public class MusicQuestion : Question
+    {
+        public string FileName { get; set; }
+
+        public override string GetAdditionalInfo()
+        {
+            return FileName; // File name for music questions
+        }
+    }
+}
---
+++ 1 KURSAS/1 SEMETRAS/OP1/L5/U5-17/U5-17/U5-17/Question.cs
@@ -0,0 +1,23 @@
+﻿namespace U5_17
+{
+    public class Question
+    {
+        public string Theme { get; set; }
+        public string Difficulty { get; set; }
+        public string Author { get; set; }
+        public string QuestionText { get; set; }
+        public string CorrectAnswer { get; set; }
+        public int Points { get; set; }
+
+        // Method for formatted table output
+        public virtual string ToTableRow()
+        {
+            return $"{Theme,-15} | {Difficulty,-12} | {Author,-10} | {QuestionText,-50} | {CorrectAnswer,-25} | {Points,-10} | {GetAdditionalInfo()}";
+        }
+
+        public virtual string GetAdditionalInfo()
+        {
+            return "-"; // Default value for OPEN questions
+        }
+    }
+}
